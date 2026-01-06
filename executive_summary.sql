8fa3c22e6a8b845099ea99173df3fed9456f02ec
Bharath Syamala <bharath.syamala@gmail.com>
2026-01-06 10:59:08 +0530

    Update executive_summary.sql

--- executive_summary.sql
+++ executive_summary.sql
@@ -7,18 +7,19 @@ select
     'EUR ' || to_char(sum(sales), 'FM999,999,999') as "Total Revenue",
     to_char(sum(quantity), 'FM999,999,999') as "Total Units Sold",
     to_char(count(order_number), 'FM999,999,999') as "Total Orders"
-from gold.fact_sales2;
+from gold.fact_sales;
 
 
 -- 2. plots - for 5 years
 -- yearly sales/revenues
+
 
 SELECT
     extract(year from order_date) as "Year",
     'EUR ' || to_char(sum(sales), 'FM999,999,999') as "Yearly Revenue",
     to_char(sum(quantity), 'FM999,999,999') as "Total Units Sold",
     to_char(count(order_number), 'FM999,999,999') as "Total Orders"
-from gold.fact_sales2
+from gold.fact_sales
 where extract(year from order_date) is not NULL
 GROUP BY extract(year from order_date);
 
@@ -29,7 +30,7 @@ SELECT
     p.category,
     p.sub_category,
     'EUR ' || to_char(sum(sales), 'FM999,999,999') as "Yearly Revenue" 
-from gold.fact_sales2 s
+from gold.fact_sales s
     join gold.dim_products p on s.product_id=p.product_id
 GROUP BY p.category, p.sub_category
 ORDER BY p.category, p.sub_category;
@@ -38,7 +39,7 @@ ORDER BY p.category, p.sub_category;
 SELECT
     p.category,
     'EUR ' || to_char(sum(sales), 'FM999,999,999') as "Yearly Revenue" 
-from gold.fact_sales2 s
+from gold.fact_sales s
     join gold.dim_products p on s.product_id=p.product_id
 GROUP BY p.category
 ORDER BY p.category;
@@ -47,14 +48,14 @@ ORDER BY p.category;
 SELECT
     p.sub_category,
     'EUR ' || to_char(sum(sales), 'FM999,999,999') as "Yearly Revenue" 
-from gold.fact_sales2 s
+from gold.fact_sales s
     join gold.dim_products p on s.product_id=p.product_id
 where s.order_date BETWEEN '2012-01-01' and '2012-12-31'
 GROUP BY p.sub_category
 ORDER BY p.sub_category;
 
+-- revenues by age group - pie chart
 
--- revenues by age group
 
 with X as (
     SELECT
@@ -86,19 +87,20 @@ SELECT
     y.age_class,
     to_char(sum(s.quantity), 'FM999,999') as total_units_sold
 from Y y
-    join gold.fact_sales2 s on s.customer_key=y.customer_key
+    join gold.fact_sales s on s.customer_key=y.customer_key
 -- where order_date BETWEEN '2013-01-01' and '2013-12-31'
 GROUP BY y.age_class
 order BY y.age_class;
 
 
--- revenues by country
+-- revenues by country - map
 
 SELECT
     c.country,
     'EUR ' || to_char(sum(s.sales), 'FM999,999,999') as revenue_by_country,
     to_char(sum(s.quantity), 'FM999,999,999') as units_sold_in_country,
     to_char(count(s.order_number), 'FM999,999,999') as no_of_orders_by_country
-from gold.fact_sales2 s
+from gold.fact_sales s
 join gold.dim_customers c on c.customer_key=s.customer_key
 GROUP BY c.country;
+
